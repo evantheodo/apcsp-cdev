4f58e28c98729f140395b6fcd5cf62a3089112de
Evan Quintana <riovan.qqq@gmail.com>
2021-01-20 02:39:11 +0000

    yeahhhhhh

--- proj/encrypt/encrypt.c
+++ proj/encrypt/encrypt.c
@@ -22,6 +22,14 @@ char shiftChar(char c, int shift, int direction)
   //   shiftChar('c', 3, 1) : 'f'
   //   shiftChar('S', 2, 0) : 'P'
   //   shiftChar('b', 3, 0) : '8'
+  if (direction = 1)
+  {
+    c = c + shift;
+  }
+  else
+  {
+    c = c - shift;
+  }
 }
 
 
